f0f38ba0cadf4334db1dd98aae8000abdf719182
Tonya <ec2-user@ip-172-31-25-65.us-east-2.compute.internal>
2020-01-24 14:55:22 +0000

    add articles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,6 +15,7 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(
       :email,
+      :current_password,
       :password,
       :password_confirmation,
       :username,
